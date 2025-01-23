1d2be6d259247dd579fe1ef8b8695b5e99afaca7
Alex <alex@alex.es>
2025-01-23 10:29:32 +0100

    -errores

--- src/main/java/com/video/MediaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.video;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MediaRepository extends JpaRepository<MediaFile, Long> {
-    MediaFile findByUrl(String url);
-
-    // void deleteById(String id);
-}
